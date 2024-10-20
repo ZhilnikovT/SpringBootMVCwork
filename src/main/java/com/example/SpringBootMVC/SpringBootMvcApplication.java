c524fc4096f399063722d14a9aeba9252868a77f
Тимур <@DESKTOP-QSL4335.mshome.net>
2024-10-20 18:19:46 +0300

    Version 1.0

--- src/main/java/com/example/SpringBootMVC/SpringBootMvcApplication.java
+++ src/main/java/com/example/SpringBootMVC/SpringBootMvcApplication.java
@@ -1,9 +1,16 @@
 package com.example.SpringBootMVC;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
-@SpringBootApplication
+//@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan(basePackages = {"com.example.SpringBootMVC", "com.example.SpringBootMVC.model"})
 public class SpringBootMvcApplication {
 
 	public static void main(String[] args) {
